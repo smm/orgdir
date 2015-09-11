0444f02004db5fae755e4771784cf8ada959d964
smm <scott@scott-martin.com>
2015-09-11 11:46:10 -0400

    added new Views, generated new user data

--- www/js/app.js
+++ www/js/app.js
@@ -2,38 +2,46 @@
 (function () {
 
     /* ---------------------------------- Local Variables ---------------------------------- */
-	var homeTpl = Handlebars.compile($("#home-tpl").html());
-	var employeeListTpl = Handlebars.compile($("#employee-list-tpl").html());
-	var service = new EmployeeService();
+    HomeView.prototype.template = Handlebars.compile($("#home-tpl").html());
+    EmployeeListView.prototype.template = Handlebars.compile($("#employee-list-tpl").html());
+    EmployeeView.prototype.template = Handlebars.compile($("#employee-tpl").html());
+
+    var service = new EmployeeService();
+    var slider = new PageSlider($('body'));
     service.initialize().done(function () {
-        renderHomeView();
+        router.addRoute('', function() {
+            console.log('empty');
+            slider.slidePage(new HomeView(service).render().$el);
+        });
+
+        router.addRoute('employees/:id', function(id) {
+            console.log('details');
+            service.findById(parseInt(id)).done(function(employee) {
+                slider.slidePage(new EmployeeView(employee).render().$el);
+            });
+        });
+
+        router.start();
     });
 
     /* --------------------------------- Event Registration -------------------------------- */
-	document.addEventListener('deviceready', function () {
-	  FastClick.attach(document.body);	
-	  if (navigator.notification) { // Override default HTML alert with native dialog
-		  window.alert = function (message) {
-			  navigator.notification.alert(
-				  message,    // message
-				  null,       // callback
-				  "Workshop", // title
-				  'OK'        // buttonName
-			  );
-		  };
-	  }
-	}, false);
+    document.addEventListener('deviceready', function () {
+        StatusBar.overlaysWebView( false );
+        StatusBar.backgroundColorByHexString('#ffffff');
+        StatusBar.styleDefault();
+        FastClick.attach(document.body);
+        if (navigator.notification) { // Override default HTML alert with native dialog
+            window.alert = function (message) {
+                navigator.notification.alert(
+                    message,    // message
+                    null,       // callback
+                    "Workshop", // title
+                    'OK'        // buttonName
+                );
+            };
+        }
+    }, false);
 
-/* ---------------------------------- Local Functions ---------------------------------- */
-    function findByName() {
-        service.findByName($('.search-key').val()).done(function (employees) {
-        	$('.content').html(employeeListTpl(employees));    
-        });
-    }
-
-	function renderHomeView() {
-		$('body').html(homeTpl());
-		$('.search-key').on('keyup', findByName);
-	}	
+    /* ---------------------------------- Local Functions ---------------------------------- */
 
 }());
